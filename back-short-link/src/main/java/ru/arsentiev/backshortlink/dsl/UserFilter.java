d93b11b7730f0c1f9214043399ee74d03da40dd6
NikitaArsentiev <necit717@gmail.com>
2024-07-22 15:00:39 +0300

    add files for pagination

--- back-short-link/src/main/java/ru/arsentiev/backshortlink/dsl/UserFilter.java
+++ back-short-link/src/main/java/ru/arsentiev/backshortlink/dsl/UserFilter.java
@@ -1,8 +1,5 @@
 package ru.arsentiev.backshortlink.dsl;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @NoArgsConstructor
