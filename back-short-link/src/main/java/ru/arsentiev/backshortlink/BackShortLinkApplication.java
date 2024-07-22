32d91e92d42336e6d45b0f4ab6c7f1c073a6d2f1
NikitaArsentiev <necit717@gmail.com>
2024-07-22 14:29:33 +0300

    add default project

---
+++ back-short-link/src/main/java/ru/arsentiev/backshortlink/BackShortLinkApplication.java
@@ -0,0 +1,13 @@
+package ru.arsentiev.backshortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackShortLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackShortLinkApplication.class, args);
+    }
+
+}
---
+++ back-short-link/src/test/java/ru/arsentiev/backshortlink/BackShortLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arsentiev.backshortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackShortLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
