f3d35b10dc7c38cc05fc145f8b0b259132d2c350
NikitaArsentiev <necit717@gmail.com>
2024-07-25 01:34:31 +0300

    add activate account page

--- ui-short-link/src/app/app-routing.module.ts
+++ ui-short-link/src/app/app-routing.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {RegisterComponent} from './pages/register/register.component';
+import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
+
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./app.component').then(m => m.AppComponent) },
-  { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'activate-account',
+    component: ActivateAccountComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
