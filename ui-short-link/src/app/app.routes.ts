0fe4d9c8473a7df8b1905a7ee96f49800c94a888
NikitaArsentiev <necit717@gmail.com>
2024-07-26 18:00:58 +0300

    add app.routes.ts

---
+++ ui-short-link/src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import {Routes} from '@angular/router';
+import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {authGuard} from "./services/guard/auth.guard";
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'activate-account',
+    component: ActivateAccountComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'links',
+    loadChildren: () => import('./modules/link/link.module')
+      .then(m => m.LinkModule),
+    canActivate: [authGuard]
+  }
+];
